547dc5a8459551ee64e4245f2b609458cc76de86
HenkSeed <adaner@inbox.ru>
2023-12-30 00:06:23 +0700

    Added Seminar02 Task01 solution

--- Seminar_02HW/1.js
+++ Seminar_02HW/1.js
@@ -19,8 +19,106 @@
 книга в списке или нет.
 */
 
+// Всё-таки добавить книгу без автора, только по названию - диссонанс.
+// Также, как и автор и название в одной строке.
+// Можно Map сделать, где название будет ключом, но в задании "массив" написано...
+// Усложню задачу, если можно.
+
+// Функция должна отработать до того, как будет создан объект library
+function bookIsDouble(book, array) {
+	const repeatitions = array.filter(
+		(item) => item.title === book.title && item.author === book.author
+	).length;
+	if (repeatitions > 1) return true;
+}
+
 class Library {
+	#books;
 	constructor(booksList) {
 		this.booksList = booksList;
+		try {
+			booksList.forEach((element) => {
+				if (bookIsDouble(element, booksList)) {
+					throw new Error('Обнаружен повтор !');
+				}
+			});
+			this.#books = booksList.slice(0);
+		} catch (err) {
+			return err;
+		}
 	}
+
+	get allBooks() {
+		return this.#books; // Returns booklist
+	}
+
+	hasBook(book) {
+		const repeatitions = this.#books.filter(
+			(item) => item.title === book.title && item.author === book.author
+		).length;
+		if (repeatitions !== 0) return true;
+	}
+
+	addBook(book) {
+		try {
+			if (this.hasBook(book)) {
+				throw new Error('Такая книга уже есть в библиотеке');
+			}
+		} catch (err) {
+			console.log('err: ', err.message);
+			return err;
+		}
+		this.#books.push(book);
+	}
+
+	removeBook(book) {
+		try {
+			if (!this.hasBook(book)) {
+				throw new Error('Такой книги нет в библиотеке');
+			}
+		} catch (err) {
+			console.log('err: ', err.message);
+			return err;
+		}
+		for (let index = 0; index < this.#books.length; index++) {
+			if (
+				this.#books[index].title === book.title &&
+				this.#books[index].author === book.author
+			) {
+				this.#books.splice(index, 1);
+			}
+		}
+	}
+
+	// hasBook(book){
+	//    return
+	// }
 }
+
+const booksList = [
+	{ title: 'Граф Монте-Кристо', author: 'Александр Дюма' },
+	// { title: 'Метро 2035', author: 'Дмитрий Глуховский' }, // Повтор
+	{ title: 'Метро 2035', author: 'Дмитрий Глуховский' },
+	{ title: 'Путешествия Гулливера', author: 'Джонатан Свифт' },
+	// { title: 'Собор Парижской Богоматери', author: 'Виктор Гюго' },
+	// { title: 'Война и мир', author: 'Лев Толстой' },
+];
+
+const library = new Library(booksList);
+console.log('library: ', library);
+
+console.log('Распечатали список книг');
+console.log('library.allBooks: ', library.allBooks);
+
+console.log('Добавляем книгу');
+// library.addBook({ title: 'Метро 2035', author: 'Дмитрий Глуховский' }); // Is in the list
+library.addBook({
+	title: 'Собор Парижской Богоматери',
+	author: 'Виктор Гюго',
+}); // Not in the list
+console.log('library.allBooks: ', library.allBooks);
+
+console.log('Удаляем книгу');
+// library.removeBook({ title: 'Метро 2035', author: 'Дмитрий Глуховский' }); // Is in the list
+library.removeBook({ title: 'Война и мир', author: 'Лев Толстой' }); // Not in the list
+console.log('library.allBooks: ', library.allBooks);
