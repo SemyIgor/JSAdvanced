6564f5c95e438814bf03fff91639c94419b8f951
HenkSeed <adaner@inbox.ru>
2024-01-05 15:32:31 +0700

    Exchenged 2.js and 2+.js files by names and deleted commented lines

--- Seminar_02HW/2.js
+++ Seminar_02HW/2.js
@@ -27,53 +27,40 @@
 
 const initialData = [
 	{
-		// id: Date.now(),
-		id: idGenerator(),
+		id: Date.now(),
 		product: 'Apple iPhone 13',
 		reviews: [
 			{
-				// id: Date.now(),
-				id: idGenerator(),
+				id: Date.now(),
 				text: 'Отличный телефон! Батарея держится долго.',
 			},
 			{
-				// id: Date.now(),
-				id: idGenerator(),
+				id: Date.now(),
 				text: 'Камера супер, фото выглядят просто потрясающе.',
 			},
 		],
 	},
 	{
-		// id: Date.now(),
-		id: idGenerator(),
+		id: Date.now(),
 		product: 'Samsung Galaxy Z Fold 3',
 		reviews: [
 			{
-				// id: Date.now(),
-				id: idGenerator(),
+				id: Date.now(),
 				text: 'Интересный дизайн, но дорогой.',
 			},
 		],
 	},
 	{
-		// id: Date.now(),
-		id: idGenerator(),
+		id: Date.now(),
 		product: 'Sony PlayStation 5',
 		reviews: [
 			{
-				// id: Date.now(),
-				id: idGenerator(),
+				id: Date.now(),
 				text: 'Люблю играть на PS5, графика на высоте.',
 			},
 		],
 	},
 ];
-
-function idGenerator() {
-	return Math.round(Math.random() * 1000000000);
-}
-
-console.log('initialData: ', initialData);
 
 // ==== Lines to copy-paste for testing ======================================================
 const less50 = 'alias exercitationem a accusantium';
@@ -93,55 +80,46 @@ const mainPage = document.querySelector('.main-page');
 productsOutput();
 
 function addReview(numb) {
-	// Сохраняем в переменную адрес полей ввода отзыва
 	const inputField = document.querySelectorAll('.review-input');
-	// Очищаем поля вывода сообщения об ошибке ввода
 	const errorMessagePlace = document.querySelectorAll('.error-message-place');
 	errorMessagePlace.forEach((elem) => {
 		elem.textContent = '';
 	});
 
-	const inputString = inputField[numb].value;
-	if (inputString.length < 50 || inputString.length > 500) {
-		throw new Error('Ошибка ввода');
+	try {
+		const inputString = inputField[numb].value;
+		if (inputString.length < 50 || inputString.length > 500) {
+			throw new Error('Ошибка ввода');
+		}
+		const reviewToAdd = { id: Date.now(), text: inputString };
+		initialData[numb].reviews.push(reviewToAdd);
+		productsOutput();
+	} catch (err) {
+		errorMessagePlace[numb].textContent = err.message;
 	}
-	// const reviewToAdd = { id: Date.now(), text: inputString };
-	const reviewToAdd = { id: idGenerator(), text: inputString };
-	initialData[numb].reviews.push(reviewToAdd);
-	productsOutput();
-
-	// try {
-	// 	const inputString = inputField[numb].value;
-	// 	if (inputString.length < 50 || inputString.length > 500) {
-	// 		throw new Error('Ошибка ввода');
-	// 	}
-	// 	const reviewToAdd = { id: Date.now(), text: inputString };
-	// 	initialData[numb].reviews.push(reviewToAdd);
-	// 	productsOutput();
-	// } catch (err) {
-	// 	errorMessagePlace[numb].textContent = err.message;
-	// }
 }
 
 function productsOutput() {
 	mainPage.innerHTML = ``;
 	initialData.forEach((item) => {
-		// Формируем список элементов <li></li> в виде шаблонной строки (отзывы на один товар)
+		const oneCard = document.createElement('oneCard');
+
 		let reviewsList = ``;
+
 		item.reviews.forEach(
 			(review) => (reviewsList += '<li class="review">' + review.text + '</li>')
 		);
-		// Формируем и размещаем на странице карточку одного товара со списком отзывов по нему из переменной reviewsList
-		mainPage.insertAdjacentHTML(
-			'beforeend',
-			`<div class="product">
-		   <h2 class="product-name">${item.product}</h2>
-		   <ul class="reviews-list">${reviewsList}</ul>
-		   <input type="text" class="review-input" placeholder="Введите отзыв">
-			<span class="error-message-place"></span><br>
-		   <button class="check-button" attr="${item.id}">Опубликовать</button>
-		 </div>`
-		);
+
+		oneCard.innerHTML = `
+	 <div class="product">
+      <h2 class="product-name">${item.product}</h2>
+      <ul class="reviews-list">${reviewsList}</ul>
+      <input type="text" class="review-input" placeholder="Введите отзыв">
+		<span class="error-message-place"></span><br>
+      <button class="check-button">Опубликовать</button>
+    </div>
+	`;
+		mainPage.append(oneCard);
 	});
 
 	const buttonCheck = document.querySelectorAll('.check-button');
